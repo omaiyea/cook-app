6427c6e06871572ae2a743719309a9f62b247895
omaiyea <omaiyea@gmail.com>
2019-07-10 00:27:24 -0700

    refactor question and answer variables

--- datastore.js
+++ datastore.js
@@ -1,44 +1,84 @@
-/*contains static text / buttons used throught app so index.js is easier to read*/
+/******* this file contains static text / buttons used throught app so index.js is easier to read*/
 
-const MAX_RESULTS = 5; //count in zomato API
+//max results to display to user and gather from api
+const MAX_RESULTS = 5; 
 
 //only checking this in because it's a graded project
 const BASE_URL_RECIPE = 'https://api.edamam.com/search?';
 const app_id_recipe = 'c83687e4'; //app_id
 const app_key_recipe = '12c994934055b08b39c25e680c1e6973'; //app_key
 //https://api.edamam.com/search?app_id=c83687e4&app_key=12c994934055b08b39c25e680c1e6973&q=chicken&q=tomatoes&dietLabels=low-carb&excluded=beef
+let recipe_api_call = ''; //to be built in index.js
 
 const api_key_restaurant = 'e7814bee053517644a136b3cc15ea4a9'; //user-key
+let restaurant_api_call = ''; //to be built in index.js
 
-//welcome screen vars
+//button value for values that won't convey inputs from user
+//will be changed using jQuery depending when it's needed
+const BUTTON = `<form><input type="submit"></form>`;
+
+//creating a separate value for next button since it's used so frequently
+const NEXT_BUTTON = `<input type="submit" value="Next">`;
+
+/*welcome screen vars*/
 const APP_TITLE = `<h2>(What) Should I Cook?</h2>`;
 const WELCOME_MESSAGE = `<p>Hangry and indecisive?</p>
 <p>Stop endlessly scrolling through the same food pictures until your blood sugar is too low for you to get up and grab your phone charger!</p>
 <p>Let's figure out what you should cook, or if you should just order takeout again.</p>
 <p>Answer some easy questions. Then we'll show you some awesome recipes.</p>
 <p>If you can't decide, we'll show you some of the best places to eat out instead!</p>`;
-const QUESTION_HELPER = `<sub>If any questions don't apply, just skip! If more than one answer is true, select them all</sub>`;
+const QUESTION_HELPER = `<sub>If any questions don't apply, just skip! If more than one answer is true, select them all.</sub>`;
 
-//button value for values that won't convey inputs from user
-//will be changed using jQuery depending when it's needed
-const BUTTON = `<form><input type="button"></form>`;
-
-//vars to get user's preferences
-
-//field are parameters expected for each API
-const QUESTIONS = [
-    {question: "First things first, are you on any diets?", field: "healthLabels", options: ["vegan", "vegetarian", "paleo", "pescatarian", "gluten-free", "low-sugar"]},
-    //future iteration: combine these dietLabels with the healthLabels question - probably makes more sense to user
-    //field: "dietLabels", options: ["balanced", "high-protein", "high-fiber", "low-fat", "low-carb", "low-sodium"]
-    {question: "And are you craving any cuisines?", field: "cuisineType", options: ["chinese", "indian", "american", "mexican", "ghanaian", "filipino"]},
-    {question: "Which ingredients do you want to use?", field: "q", options: ["chicken", "beef", "broccoli", "cauliflower", "cheese"]},
-    {question: "Are there ingredients that you absolutely hate or can't eat?", field: "excluded", options: ["soy", "pork", "peanuts", "dairy", "eggs", "shellfish", "sesame"]},
-    {question: "And last, where do you eat out?", field: "q", options: []} //seems kind of hacky, leave options blank and then look for a blank array to get user input for q=USER's CITY
-];
+/*vars to get user's preferences*/
 
 let QUESTION_COUNTER = 0; //loops through questions
 
-const STATE = `<select>
+//array for questions since they're least likely to change
+const QUESTIONS = [
+    {question: "First things first, are you on any diets?", type: "yesNo"}, 
+    {question: "And are you craving any cuisines?", type: "yesNo"},
+    {question: "Which ingredients do you want to use?", type: "multiChoice"},
+    {question: "Are there ingredients that you can't eat?", type: "multiChoice"},
+    {question: "And last, where do you eat out?", type: "location"}
+];
+
+//used to generate yes/no answers for the yes/no questions
+const YES_NO_RADIO = `<label>Yes<input type="radio" name="yesNo" value="Yes"></label>
+<label>No<input type="radio" name="yesNo" value="No"></label>`;
+
+//object for each answer to be mapped to questions and query parameters to get data from differentn apis
+//made this way since answers and api parameters may change 
+//also since different APIs require different parameters/names for same type of parameter
+const HEALTH_ANSWERS = {
+    paramRecipe: "healthLabels", 
+    options: ["vegan", "vegetarian", "paleo", "pescatarian", "gluten-free", "keto", "low-sugar"]
+};
+
+//to end user diet and health are likely the same but in API they are treated differently
+//next iteration figure out how to do this 
+/*const DIET_ANSWERS = {
+    paramRecipe: "dietLabels",
+    options: ["balanced", "high-protein", "high-fiber", "low-fat", "low-carb", "low-sodium"]
+};*/
+
+const CUISINE_ANSWERS = {
+    paramRecipe: "cuisineType", 
+    paramRestaurant: "cuisine", 
+    options: ["chinese", "indian", "american", "mexican", "ghanaian", "filipino"]
+};
+
+const INGREDIENTS = {
+    paramRecipe: "q", 
+    options: ["chicken", "beef", "broccoli", "cauliflower", "cheese"]
+};
+
+//commonly excluded ingredients so separate data structure than commonly used ingredients
+const EXCLUDED_INGREDIENTS = {
+    paramRecipe: "excluded", 
+    options: ["soy", "pork", "peanuts", "dairy", "eggs", "shellfish", "sesame"]
+};
+const CITY = `<label for="city">City: <input type="text"></input></label>`;
+const STATE = `<label for="state">State: <select id="state">
     <option value="AL">Alabama</option>
     <option value="AK">Alaska</option>
     <option value="AZ">Arizona</option>
@@ -90,6 +130,21 @@ const STATE = `<select>
     <option value="WV">West Virginia</option>
     <option value="WI">Wisconsin</option>
     <option value="WY">Wyoming</option>
-</select>`;
+</select></label>`;
+
+const LOCATION = {
+    param: "q",
+    state: STATE
+};
+
+//maps questions and answers
+const QUESTIONS_AND_ANSWERS = [
+    {qid: 0, answer: HEALTH_ANSWERS},
+    {qid: 1, answer: CUISINE_ANSWERS},
+    {qid: 2, answer: INGREDIENTS},
+    {qid: 3, answer: EXCLUDED_INGREDIENTS},
+    {qid: 4, answer: LOCATION}
+];
 
 //vars to display user's preferences
+
--- index.js
+++ index.js
@@ -4,39 +4,81 @@ function renderApp(){
     $('.title').html(APP_TITLE);
     $('.subtitle').html(WELCOME_MESSAGE);
     $('.food-preferences').html(BUTTON);
-    $('input[type="button"').prop('value', "Let's Start!");
-    $('form').prop('class', "preferences");   //Need to change to iterate through questions
+    $('input[type="submit"]').prop('value', "Let's Start!");
 }
 
-//load questions on user's food preferences when clicked
+//display question to gather user's food preferences when clicked
 function handlePrefs(){
-    //need to change to iterate through questions
-    $('.food-preferences').on('click', '.preferences', function(event){
+    $('.food-preferences').on('click', 'input[type="submit"]', function(event){
         event.preventDefault();
         console.log('handlePrefs ran');
         $('.subtitle').html(QUESTION_HELPER);
         $('.food-preferences').html(renderQns());
+        QUESTION_COUNTER++;
     });
 }
 
-//generate questions
+//generate the question to be displayed above
+//values of question depend on what type of question it is
 function renderQns(){
-    let prefsForm = `<form id="${QUESTIONS[QUESTION_COUNTER].field}" class="radio-buttons">
-        <legend>First things first, are you on any diets?</legend>`;
-    for(i = 0; i<(QUESTIONS[QUESTION_COUNTER].options).length; i++){
-        prefsForm += `<label for="${QUESTIONS[QUESTION_COUNTER].options[i]}">
-        <span>${QUESTIONS[QUESTION_COUNTER].options[i]}</span>
-        <input type="checkbox" id="${QUESTIONS[QUESTION_COUNTER].options[i]}" type="${QUESTIONS[QUESTION_COUNTER].field}"> 
-        </label>`;
+    let question = '';
+    if(QUESTIONS[QUESTION_COUNTER].type === "yesNo"){
+        console.log(`rendering a yes/no question`);
+        question += `<form id="${QUESTIONS[QUESTION_COUNTER].type}">
+        <legend>${QUESTIONS[QUESTION_COUNTER].question}</legend>`;
+        question += YES_NO_RADIO;
+        question += NEXT_BUTTON + `</form>`;
     }
-    prefsForm += `<input type="submit" value="Next!"></form>`;
-    QUESTION_COUNTER++;
-    return prefsForm;
+    if (QUESTIONS[QUESTION_COUNTER].type === "multiChoice"){
+        console.log(`rendering a multiple choice question`);
+        question = generateMultipleChoices(); //defined as a function so this can be used for follow up questions yes/no questions
+    }
+    if (QUESTIONS[QUESTION_COUNTER].type === "location"){
+        console.log(`rendering a location question`);
+        question = `<form id="${QUESTIONS[QUESTION_COUNTER].type}" class="text-inputs">
+        <legend>${QUESTIONS[QUESTION_COUNTER].question}</legend>`;
+        question += CITY;
+        question += STATE;
+        question += NEXT_BUTTON + `</form>`;
+    }
+    return question;
 }
+
+//generate data for questions with multiple choices
+//will also be used for questions with potential follow up questions depending on yes/no answer
+function generateMultipleChoices(){
+    let optionsHTML = '';
+    console.log('rendering multiple choice questions');
+    //need to change ID since field isn't unique
+    optionsHTML = `<form id="${QUESTIONS[QUESTION_COUNTER].type}" class="radio-buttons">
+    <legend>${QUESTIONS[QUESTION_COUNTER].question}</legend>`;
+        //find the question that we're on in the questions_and_answers variable
+        //then find the options for potential answers for that question 
+        //then display those options in a form
+    QUESTIONS_AND_ANSWERS.find(a => a.qid === QUESTION_COUNTER).answer.options.forEach(function(option){
+        optionsHTML += `<label for="` + option + `"><span>` + option + `</span>
+        <input type="checkbox" id="` + option + `" type="${QUESTIONS[QUESTION_COUNTER].type}"></label>`;
+    });
+    optionsHTML += NEXT_BUTTON + `</form>`;
+    return optionsHTML;
+}
+
+//listens for when user selects a yes/no question  
+//displays additional questions if the answer is yes 
+//todo: save value to be used in API 
+//submit event also doens't work
+/*function handleYesNo(){
+    $('.food-preferences').on('submit', '#yesNo', function(event){
+        event.preventDefault();
+        let userAnswer = $('input[type="radio"]:checked').val();
+        console.log(userAnswer);
+    });
+}*/
 
 function handleCookApp(){
     renderApp();
     handlePrefs();
+   // handleYesNo();
 }
 
 //run after the page loads
